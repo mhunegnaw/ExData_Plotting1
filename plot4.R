e198c6851f2621a1e2e16954b141e96549bea3a4
Molla Hunegnaw Asmare <mhunegnaw@gmail.com>
2014-05-12 00:45:10 +0200

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,20 @@
+## Author: Molla Hunegnaw 
+## plot4.R
+mydata <- read.table('household_power_consumption.txt', sep=';', header=T, 
+                     colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'), na.strings='?')
+mydata$DateTime <- strptime(paste(mydata$Date, mydata$Time), "%d/%m/%Y %H:%M:%S")
+mydata <- subset(mydata, as.Date(DateTime) >= as.Date("2007-02-01") &  as.Date(DateTime) <= as.Date("2007-02-02"))
+png("plot4.png", height=480, width=480)
+par(mfrow=c(2,2))
+plot(mydata$DateTime,      mydata$Global_active_power,pch=NA,xlab="",ylab="Global Active Power")
+lines(mydata$DateTime, mydata$Global_active_power)
+plot(mydata$DateTime, mydata$Voltage, ylab="Voltage", xlab="datetime", pch=NA)
+lines(mydata$DateTime, mydata$Voltage)
+plot(mydata$DateTime,      mydata$Sub_metering_1,pch=NA,xlab="",ylab="Energy sub metering")
+lines(mydata$DateTime, mydata$Sub_metering_1)
+lines(mydata$DateTime, mydata$Sub_metering_2, col='red')
+lines(mydata$DateTime, mydata$Sub_metering_3, col='blue')
+legend('topright',        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),        lty = c(1,1,1),       col = c('black', 'red', 'blue'),       bty = 'n')
+with(mydata, plot(DateTime, Global_reactive_power, xlab='datetime', pch=NA))
+with(mydata, lines(DateTime, Global_reactive_power))
+dev.off()
